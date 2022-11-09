1c8a91a01c67f1034d0c4851090e5b97ae9a8eea
Joey Zhou <zhouxinyi99@163.com>
2022-11-09 08:21:50 -0500

    delete batch

--- batch/None_dur2/sav.m
+++
@@ -1,9 +0,0 @@
-% List of open inputs
-nrun = X; % enter the number of runs here
-jobfile = {'E:\OneDrive Local\OneDrive - Johns Hopkins\Desktop\Lab\code\model-free\batch\None_dur2\sav_job.m'};
-jobs = repmat(jobfile, 1, nrun);
-inputs = cell(0, nrun);
-for crun = 1:nrun
-end
-spm('defaults', 'FMRI');
-spm_jobman('run', jobs, inputs{:});
