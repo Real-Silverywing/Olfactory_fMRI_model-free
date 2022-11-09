9569299b80cb3d024e047bcc9ae62b24df8db5e8
Joey Zhou <zhouxinyi99@163.com>
2022-11-09 14:10:50 -0500

    Add default threshold

--- FIR/small_std_helper.m
+++ FIR/small_std_helper.m
@@ -1,8 +1,15 @@
-function [removed_tc,idx] = small_std_helper(data)
-%help to remove small std time course
-%   Detailed explanation goes here
-stds = std(data,0,2);
-idx = find(stds>=1);
-removed_tc = data(stds>=1,:);
+function [removed_tc,idx] = small_std_helper(data, std_thres)
+%help to remove the timecourses with small std(nearly constant)
+% defalut threshold is 0.5
+if nargin==1
+    std_thres = 0.5;
+    stds = std(data,0,2);
+    idx = find(stds>=std_thres);
+    removed_tc = data(stds>=std_thres,:);
+elseif nargin==2
+    stds = std(data,0,2);
+    idx = find(stds>=std_thres);
+    removed_tc = data(stds>=std_thres,:);
+
 end
 
